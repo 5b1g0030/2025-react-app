dd3c56918781241561e506c8d1d64daeef6d5dfd
5b1g0030 <5b1g0030@stust.edu.tw>
2025-05-07 16:39:41 +0800

    添加註解

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+// -----匯入模組資源
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MySlider from "./component/MySlider"; // 根據檔案路徑調整
 
+// App()是整個畫面的核心組件，React 會從這裡開始渲染畫面。
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0) //建立一個狀態變數 count，每次呼叫它React都會被重新渲染
 
-  return (
+  return ( // 回傳值(畫面內容)
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
@@ -35,4 +37,5 @@ function App() {
   )
 }
 
+// 讓這個 App 組件能被其它地方匯入使用（例如 main.jsx 中的 <App />）
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// 渲染到我網頁上的這個 <div id="root"> 裡面
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
