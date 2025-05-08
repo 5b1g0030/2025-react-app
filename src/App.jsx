fe5e3ecf323af74700bef536329ae8dae49e2d03
5b1g0030 <5b1g0030@stust.edu.tw>
2025-05-08 09:45:57 +0800

    完成RGB Slider元件布置

--- src/App.jsx
+++ src/App.jsx
@@ -21,9 +21,12 @@ function App() {
       </div>
       <h1>本網站使用Vite + React開發</h1>
       <h2>我的第一個元件</h2>
-      <p>R:<MySlider /></p>
-      <p>G:<MySlider /></p>
-      <p>B:<MySlider /></p>
+      <div>
+        <p>R:<MySlider /></p>
+        <p>G:<MySlider /></p>
+        <p>B:<MySlider /></p>
+      </div>
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
